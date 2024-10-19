1afdf630e6e4a53579dec159839abb2941fc6e0d
SteveMama <pkompally@gmail.com>
2024-10-19 17:06:11 -0400

    --intro RoP freqs--

--- main.py
+++ main.py
@@ -20,3 +20,25 @@ print(tokens.shape, tokens)
 d = 16
 embedding = nn.Embedding(v, d)
 print(embedding.weight.shape,embedding.weight)
+
+x = embedding(tokens)
+print(x.shape, x)
+
+
+# Precopmuting RoPE Frequences
+
+theta = 10000
+num_heads = 4
+head_dim = d // num_heads
+
+freqs = 1.0 / (theta ** (torch.arange(0, head_dim, 2)[: (head_dim //2)].float() / head_dim))
+print(f'freqs: {freqs.shape}\n {freqs}\n')
+
+t = torch.arange(seq_len * 2, device = freqs.device, dtype = torch.float32)
+print(f'freqs: {freqs.shape}\n{t}\n')
+
+freqs = torch.outer(t, freqs)
+print(f'freqs: {freqs.shape}\n{freqs}\n')
+
+freqs_cis = torch.polar(torch.ones_like(freqs), freqs)[: seq_len]
+print(f'freqs_cis:{freqs_cis.shape}\n{freqs_cis}')
