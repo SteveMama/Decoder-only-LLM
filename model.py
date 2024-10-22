3f34a4f748e4daafab3c5474d9b921f71206c1d7
SteveMama <pkompally@gmail.com>
2024-10-22 16:44:15 -0400

    --updated bugs & fixed---

--- model.py
+++ model.py
@@ -29,20 +29,20 @@ class ModelArgs:
     rope_theta: float = 10000
     max_batch_size: int = 32
     max_seq_len: int = 512
-    device: str ='cuda'
+    device: str ='cpu'
     dropout_rate: float = 0.1
 
 
 # RMS Norm
 
 class RMSNorm(torch.nn.Module):
-    def __init__(self, dim: int, eps:float = 1e-6):
-        super().__init_()
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(dim))
 
     def _norm(self, x):
-        return x * torch.sqrt(x.pow(2).mean(-1, keepdim = True) + self.eps)
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
 
     def forward(self, x):
         output = self._norm(x.float()).type_as(x)
